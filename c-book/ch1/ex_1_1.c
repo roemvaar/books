8703de70a4b4bc24ac1c475fcb678d2d740ecb2d
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-09-22 11:41:59 -0500

    Update and rename hello_world.c to ex_1_1.c

---
+++ c-book/ch1/ex_1_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Exercise 1.1: When you don't specify a return type for the main function
+ * A warning is thrown by gcc 9.3.0 "warning: return type defaults to 'int'
+ * C90 used to accept main() but the C99 standard states that you should return a value.
+ *
+ * Exercise 1.1: When you don't have a main function (or when you misspell main, e.g ain),
+ * the gnu linker (ld) shows the following message 'undefined reference to 'main'
+ */
+
+
+int main()
+{
+    printf("hello, world\n");
+    printf("Ohana means family\c"); // Unknown escape sequence
+    
+    return 0;
+}
+ 
