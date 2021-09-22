f9d8b1d49975cc9a19fb9cdaeea3a25236dd4b92
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-09-22 11:43:21 -0500

    Create ex_1_2.c

---
+++ c-book/ch1/ex_1_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Exercise 1.2: When printf's argument string contains an unknown escape sequence, e.g. \c
+ * the following message is shown at compilation time "warning: unknown escape sequence: \c"
+ */
+
+int main()
+{
+    printf("hello, world\n");
+    printf("Ohana means family\c"); // Unknown escape sequence
+    
+    return 0;
+}
+ 
