3e0346565c9afbb817ad06ea559190a9eef8e129
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-10-12 01:08:52 -0500

    Create p3.c

---
+++ OSTEP/p3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+  printf("hello world (pid:%d)\n", (int) getpid());
+}
+
+ 
