108a079a0a6f3566a44b7c47fb47fe6bb86be61b
Roberto Valenzuela <valenzuelarober@gmail.com>
2024-06-21 17:27:16 -0400

    ostep: thread example

---
+++ OSTEP/t0.c
@@ -0,0 +1,36 @@
+/* Simple thread creation code
+ * $ gcc -pthread -o t0 t0.c -Wall
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
+
+
+void *mythread(void *arg)
+{
+    printf("%s\n", (char *) arg);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t p1;
+    pthread_t p2;
+
+    int rc;
+
+    printf("main: begin\n");
+
+    rc = pthread_create(&p1, NULL, mythread, "A"); assert(rc == 0);
+    rc = pthread_create(&p2, NULL, mythread, "B"); assert(rc == 0);
+
+    // Join waits for the threads to finish
+    rc = pthread_join(p1, NULL); assert(rc == 0);
+    rc = pthread_join(p2, NULL); assert(rc == 0);
+
+    printf("main: end\n");
+
+    return 0;
+}
